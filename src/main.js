119d3ffa876ea8ec6441415bb1e9a0eca390d308
rllola <rllola80@gmail.com>
2017-06-02 15:47:31 +0200

    Add electron-startup-squirrel

--- src/main.js
+++ src/main.js
@@ -39,6 +39,8 @@ function createWindow () {
   })
 }
 
+if(require('electron-squirrel-startup')) app.quit();
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
